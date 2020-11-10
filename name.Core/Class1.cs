507cd619e51c38cb6f328d90517eb3fada951b5e
Dominik Gogola <d.s.gogola@gmail.com>
2020-11-10 21:46:31 +0100

    Initial commit

---
+++ name.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace name.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ name.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace name.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
